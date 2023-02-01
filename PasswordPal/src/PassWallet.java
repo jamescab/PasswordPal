baa0d606274d181bbe92149b95a679bfd66706ef
James Cabaral <jamescabaral@gmail.com>
2023-01-31 23:56:02 -0800

    Update PassWallet
    Removed excess print statements and started implementation for reading from/writing to a file.

--- PasswordPal/src/PassWallet.java
+++ PasswordPal/src/PassWallet.java
@@ -1,15 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Properties;
 
-public class passWallet {
+public class PassWallet {
 	private Map<String,String> wallet;
 	private Scanner in = new Scanner(System.in);
+	private Properties p = new Properties();
+	private String outputFilePath = "C:/Users/james/eclipse-workspace/PasswordPal/src/wallet.txt";
 	
-	public passWallet() {
+	//Constructor
+	public PassWallet() {
 		wallet = new HashMap<String,String>();
+		try {
+			p.load(new FileInputStream("data.p"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for (String key : p.stringPropertyNames()) {
+			   wallet.put(key, p.get(key).toString());
+			}
 	}
 	
+	//Main menu for PassWallet
 	public void passWalletMenu() {
 		boolean keepAsking = true;
 		
@@ -20,6 +44,7 @@ public class passWallet {
 			System.out.println("2. Find password for an account");
 			System.out.println("3. Change a password");
 			int option = in.nextInt();
+			in.nextLine();
 			
 			switch (option) {
 			case 1:
@@ -39,23 +64,64 @@ public class passWallet {
 			System.out.println("Leave wallet? (Y or N)");
 			String response = in.nextLine();
 			if (response.equals("Y")) {
+
+				File file = new File(outputFilePath);
+				  
+		        BufferedWriter bf = null;
+		  
+		        try {
+		  
+		            // create new BufferedWriter for the output file
+		            bf = new BufferedWriter(new FileWriter(file));
+		  
+		            // iterate map entries
+		            for (Map.Entry<String, String> entry :
+		                 wallet.entrySet()) {
+		  
+		                // put key and value separated by a colon
+		                bf.write(entry.getKey() + ":"
+		                         + entry.getValue());
+		  
+		                // new line
+		                bf.newLine();
+		            }
+		  
+		            bf.flush();
+		        }
+		        catch (IOException e) {
+		            e.printStackTrace();
+		        }
+		        finally {
+		  
+		            try {
+		  
+		                // always close the writer
+		                bf.close();
+		            }
+		            catch (Exception e) {
+		            }
+		        }
 				keepAsking = false;
 			}
 		}
 	}
 	
+	//Inserts password into wallet
 	public void setPass(String account, String password) {
 		wallet.put(account, password);
 	}
 	
+	//Returns entire wallet
 	public Map<String,String> getWallet() {
 		return wallet;
 	}
 	
+	//Returns password from wallet based on account name
 	public String getPass(String account) {
 		return wallet.get(account);
 	}
 	
+	//Displays all account names and passwords in wallet
 	public void displayWallet() {
 		System.out.println("All accounts and passwords:");
 		if (!wallet.isEmpty()) {
@@ -65,6 +131,8 @@ public class passWallet {
 		}
 	}
 	
+	//Find a password in wallet based on account name
+	//Gives the option to set account name and password if account is not in wallet
 	public void findPass () {
 		System.out.print("Please enter account for password: ");
 		String checkAccount = in.nextLine();
@@ -82,6 +150,7 @@ public class passWallet {
 		}
 	}
 	
+	//Change password that already exists in wallet
 	public void changePass() {
 		System.out.print("Which account do you want to update the password for?");
 		String account = in.nextLine();
@@ -107,4 +176,5 @@ public class passWallet {
 		}
 	}
 }
+
 
